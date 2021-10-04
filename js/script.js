ba471d3bf14df5255cb13e053e1413c03959dadc
callmejogi <kutafin01@gmail.com>
2021-10-04 17:10:35 +0300

    done

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let num = prompt(`Choose the operation:+, -, *, / ?`);
+let result = console.log(`Действие:${num}`)
+
+let num1 = prompt(`Choose first number`);
+let num2 = prompt(`Choose second number`);
+
+let result1 = Number(num1) + Number(num2);
+console.log(result1)
+
+let result2 = num1 - num2;
+console.log(result2)
+
+let result3 = num1 * num2;
+console.log(result3)
+
+let result4 = num1 / num2;
+console.log(result4)
+
+
